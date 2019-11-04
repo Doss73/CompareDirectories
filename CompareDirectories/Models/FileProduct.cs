80dd2675b1ed216d07f02cadf9eecd4e116df8e8
Vladislav Panasyuk <panasiukvladyslav@gmail.com>
2019-11-04 14:54:48 +0200

    Replaced properties by autoproperty

--- CompareDirectories/Models/FileProduct.cs
+++ CompareDirectories/Models/FileProduct.cs
@@ -11,67 +11,15 @@ namespace CompareDirectories
 {
     public class FileProduct
     {
-        private string filePath;
-        private string name;
-        private string sizeWithSuffix;
-        private long sizeInBytes;
-        private DateTime date;
-        private string status;
-
-        public string FilePath
-        {
-            get { return filePath; }
-            set
-            {
-                filePath = value;
-            }
-        }
-        public string Name
-        {
-            get { return name; }
-            set
-            {
-                name = value;
-              
-            }
-        }
-        public string SizeWithSuffix
-        {
-            get { return sizeWithSuffix; }
-            set
-            {
-                sizeWithSuffix = value;
-                
-            }
-        }
-        public long SizeInBytes
-        {
-            get { return sizeInBytes; }
-            set
-            {
-                sizeInBytes = value;
-            }
-        }
-        public string Date
-        {
-            get { return date.ToShortDateString(); }
-            set
-            {
-                DateTime.TryParse(value, out date);
-            }
-        }
-        public string Status
-        {
-            get { return status; }
-            set
-            {
-                status = value;
-               
-            }
-        }
+        public string FilePath { get; set; }
+        public string Name { get; set; }
+        public string SizeWithSuffix { get; set; }
+        public long SizeInBytes { get; set; }
+        public string Date { get; set; }
+        public string Status { get; set; }
         public FileProduct(string filePath)
         {
-            this.filePath = filePath;
+            FilePath = filePath;
         }
       
     }
